453c039b0aa1cd29e879f0190e58c0f037da1370
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-09 15:19:11 -0500

    Update InsureMeApplication.java

--- src/main/java/com/yugandhar/devsecops/insureme/InsureMeApplication.java
+++ src/main/java/com/yugandhar/devsecops/insureme/InsureMeApplication.java
@@ -1,4 +1,4 @@
-pac kage com.yugandhar.devsecops.insureme;
+package com.yugandhar.devsecops.insureme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

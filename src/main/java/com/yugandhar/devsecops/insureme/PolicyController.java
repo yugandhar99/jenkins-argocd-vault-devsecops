ecae17ea209bd1f248bf85ab23442fd1be39ec24
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-09-07 22:50:11 -0500

    Update PolicyController.java

--- src/main/java/com/yugandhar/devsecops/insureme/PolicyController.java
+++ src/main/java/com/yugandhar/devsecops/insureme/PolicyController.java
@@ -1,6 +1,6 @@
 package com.yugandhar.devsecops.insureme;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
